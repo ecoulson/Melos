88ecbf22a78cfed8fad259e35e6774cd2f4c53f5
Evan Coulson <evan.m.coulson@gmail.com>
2021-12-07 20:38:44 -0800

    updated for music quiz 3

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -3,35 +3,38 @@ import IQuiz from "../data/IQuiz";
 import QuizPage from "../pages/Quiz";
 import RegionList from "./ReigonList";
 import SetPageHandler from "../SetPageHandler";
+import Region from "../data/Region";
 
 interface IQuizCardProps {
-  quiz: IQuiz;
-  setPage: SetPageHandler;
+	quiz: IQuiz;
+	setPage: SetPageHandler;
 }
 
 export default function QuizCard(props: IQuizCardProps) {
-  function handleClick() {
-    props.setPage(new QuizPage(props.quiz));
-  }
+	function handleClick() {
+		props.setPage(new QuizPage(props.quiz));
+	}
 
-  return (
-    <Card
-      elevation={1}
-      width="300px"
-      boxSizing="border-box"
-      border="default"
-      display="flex"
-      justifyContent="space-evenly"
-      alignItems="center"
-      flexDirection="column"
-    >
-      <Heading size={600} margin="25px">
-        {props.quiz.name}
-      </Heading>
-      <RegionList regions={props.quiz.regions} />
-      <Button onClick={handleClick} appearance="primary" marginY="25px">
-        Practice
-      </Button>
-    </Card>
-  );
+	return (
+		<Card
+			elevation={1}
+			width="300px"
+			boxSizing="border-box"
+			border="default"
+			display="flex"
+			justifyContent="space-evenly"
+			alignItems="center"
+			flexDirection="column"
+		>
+			<Heading size={600} margin="25px">
+				{props.quiz.name}
+			</Heading>
+			<RegionList
+				regions={props.quiz.regions.filter((x) => x !== Region.Bali)}
+			/>
+			<Button onClick={handleClick} appearance="primary" marginY="25px">
+				Practice
+			</Button>
+		</Card>
+	);
 }
--- client/src/data/Region.ts
+++ client/src/data/Region.ts
@@ -1,13 +1,14 @@
 enum Region {
-  SubsaharanAfrica = "Sub-Saharan Africa",
-  MiddleEast = "Middle East",
-  EasternEurope = "Eastern Europe",
-  LatinAmerica = "Latin America",
-  India = "India",
-  SouthAsia = "South Asia",
-  China = "China",
-  Japan = "Japan",
-  Indonesia = "Indonesia",
+	SubsaharanAfrica = "Sub-Saharan Africa",
+	MiddleEast = "Middle East",
+	EasternEurope = "Eastern Europe",
+	LatinAmerica = "Latin America",
+	India = "India",
+	SouthAsia = "South Asia",
+	China = "China",
+	Japan = "Japan",
+	CentralJava = "Central Java",
+	Bali = "Bali",
 }
 
 export default Region;
